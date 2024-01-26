d6f8d2b6727d0ffa4a2d6e19e2ae15a55296045d
edutjie <edutjitrahardja@gmail.com>
2024-01-26 17:59:30 +0700

    add metadata

--- src/components/modules/LandingModule/hero/EducationCard.tsx
+++ src/components/modules/LandingModule/hero/EducationCard.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 export const EducationCard = () => {
   return (
-    <div className="bg-[#0B3866]/25 px-[30px] py-[15px] rounded-[30px] flex flex-col gap-8 text-left backdrop-blur-md shadow-xl w-full max-w-[450px]">
+    <div
+      onClick={() => window.open('https://cs.ui.ac.id/', '_blank')}
+      className="bg-[#0B3866]/25 px-[30px] py-[15px] rounded-[30px] flex flex-col gap-8 text-left backdrop-blur-md shadow-xl w-full max-w-[450px] cursor-pointer hover:shadow-[#9BF6FF]/25 hover:shadow-2xl transition-all duration-300 ease-in-out"
+    >
       <div className="flex gap-8">
         <div className="relative w-24 h-24 self-center col-span-2 md:col-span-1">
           <Image
