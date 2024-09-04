d2ea5e661347d956678d483f259c1f32db5887ce
edutjie <edutjitrahardja@gmail.com>
2024-09-04 14:18:43 +0700

    update metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import { Footer, Navbar } from '@elements'
 
 export const metadata: Metadata = {
-  title: 'edutjie | Personal Website',
+  title: 'Eduardus Tjitrahardja | Personal Website',
   description:
     "A Computer Science student who is very passionate and interested in technology and computer especially in Artificial Intelligence. I have acquired a vast amount of knowledge in various fields of programming such as Machine Learning, Software Engineering, etc. I'm currently studying Computer Science at University of Indonesia where I can learn much and put my knowledge and experience.",
 }
